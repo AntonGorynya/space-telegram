c39bc0d1911ea59ea430776b208b6d2b5503f68c
SD <antongorynya1@gmail.com>
2023-02-06 20:57:03 +0300

    publish photo to Telegram channel. update readme and requirements

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ chat_id = '@l00k_around'
 if __name__ == '__main__':
     load_dotenv()
     bot = telegram.Bot(token=os.environ['TELEGA_KEY'])
-    bot.send_message(text='Hi John!', chat_id='@l00k_around')
+    bot.send_photo(chat_id=chat_id, photo=open('images/nasa_0.jpg', 'rb'))
